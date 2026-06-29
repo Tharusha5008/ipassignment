1488ad16b5f246f71b874aaf85582b2738d0d055
Nadunlasanka117 <pgnadunlasanka@gmail.com>
2026-06-29 19:45:44 +0530

    updated about js

--- js/app.js
+++ js/app.js
@@ -92,3 +92,21 @@ this.reset();
 });
 
 }
+const boxes=document.querySelectorAll(".box");
+
+window.addEventListener("scroll",()=>{
+
+boxes.forEach(box=>{
+
+const position=box.getBoundingClientRect().top;
+
+if(position<window.innerHeight-100){
+
+box.style.opacity="1";
+box.style.transform="translateY(0)";
+
+}
+
+});
+
+});
