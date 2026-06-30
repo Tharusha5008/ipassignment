954ea4d5e1e3adcd8450bb01c1ca2f5d2e2f1491
akalanka-epa <271993612+akalanka-epa@users.noreply.github.com>
2026-06-30 20:35:29 +0530

    update the home page

--- js/app.js
+++ js/app.js
@@ -110,3 +110,32 @@ box.style.transform="translateY(0)";
 });
 
 });
+
+const counters=document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.dataset.target;
+const count=+counter.innerText;
+
+const increment=target/100;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+}
+
+update();
+
+});
